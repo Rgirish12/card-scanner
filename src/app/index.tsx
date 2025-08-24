80a719da0c3fa6342a75a6bd4705a4eae52688d6
Girish <girishrajput154@gmail.com>
2025-08-24 23:43:59 +0530

    Add icon and splash screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,12 @@
+import * as SplashScreen from "expo-splash-screen";
 import { View, Text } from "react-native";
+
 import React from "react";
 import { Redirect } from "expo-router";
-
+SplashScreen.preventAutoHideAsync();
 const RoutIndex = () => {
+  SplashScreen.hideAsync();
+
   return <Redirect href="/(tabs)" />;
 };
 
